f3979a0f94fee2321ccf9155602b601fd955bacd
umairhameed236 <101642844+umairhameed236@users.noreply.github.com>
2024-11-29 00:53:42 +0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "terraformstaterk"           # The name of the S3 bucket
-    region = "us-east-1"                  # The AWS region where the bucket is located
+    key    = "terraform/state/terraform.tfstate"
+    
+region = "us-east-1"                  # The AWS region where the bucket is located
   }
 }
